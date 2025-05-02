b6cb83e3898f1f7e0f5ab35803a393b932c80c45
AphidZ <yogada017@gmail.com>
2025-05-02 11:06:53 +0700

    move showMenu() -> function.go

--- cmd/main.go
+++ cmd/main.go
@@ -7,25 +7,26 @@ import (
 
 // showMenu function for showing menu
 func showMenu() {
-	fmt.Println("=====================================================")
-	fmt.Println("||       SELAMAT DATANG DI APLIKASI DAUR ULANG     ||")
-	fmt.Println("=====================================================")
-	fmt.Println("||  1. Tambah data sampah                          ||")
-	fmt.Println("||  2. Ubah data sampah                            ||")
-	fmt.Println("||  3. Hapus data sampah                           ||")
-	fmt.Println("||  4. Catat proses daur ulang                     ||")
-	fmt.Println("||  5. Cari data sampah                            ||")
-	fmt.Println("||  6. Urutkan data sampah                         ||")
-	fmt.Println("||  7. Tampilkan statistik                         ||")
-	fmt.Println("||  8. Tampilkan semua data                        ||")
-	fmt.Println("||  9. Keluar program                              ||")
-	fmt.Println("=====================================================")
+	fmt.Println("======================================================")
+	fmt.Println("||   SELAMAT DATANG DI APLIKASI DAUR ULANG SAMPAH   ||")
+	fmt.Println("======================================================")
+	fmt.Println("||  1. Tambah data sampah                           ||")
+	fmt.Println("||  2. Ubah data sampah                             ||")
+	fmt.Println("||  3. Hapus data sampah                            ||")
+	fmt.Println("||  4. Catat proses daur ulang                      ||")
+	fmt.Println("||  5. Cari data sampah                             ||")
+	fmt.Println("||  6. Urutkan data sampah                          ||")
+	fmt.Println("||  7. Tampilkan statistik                          ||")
+	fmt.Println("||  8. Tampilkan semua data                         ||")
+	fmt.Println("||  9. Keluar program                               ||")
+	fmt.Println("======================================================")
 	fmt.Print("Pilih menu (1-9): ")
 }
 
 func main() {
 	var input string
 
+	// loop
 	for {
 		showMenu()
 
@@ -56,7 +57,7 @@ func main() {
 			showAllData()
 		case 9:
 			fmt.Println("Keluar dari program!")
-			return
+			return // break loop
 		default:
 			fmt.Println("Pilihan tidak valid, coba lagi.")
 		}
