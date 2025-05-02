f724d9333b79a5474e288e43e559760324c2ae1b
AphidZ <yogada017@gmail.com>
2025-05-02 11:08:29 +0700

    move showMenu() -> function.go

--- cmd/main.go
+++ cmd/main.go
@@ -5,24 +5,6 @@ import (
 	"fmt"
 )
 
-// showMenu function for showing menu
-func showMenu() {
-	fmt.Println("======================================================")
-	fmt.Println("||   SELAMAT DATANG DI APLIKASI DAUR ULANG SAMPAH   ||")
-	fmt.Println("======================================================")
-	fmt.Println("||  1. Tambah data sampah                           ||")
-	fmt.Println("||  2. Ubah data sampah                             ||")
-	fmt.Println("||  3. Hapus data sampah                            ||")
-	fmt.Println("||  4. Catat proses daur ulang                      ||")
-	fmt.Println("||  5. Cari data sampah                             ||")
-	fmt.Println("||  6. Urutkan data sampah                          ||")
-	fmt.Println("||  7. Tampilkan statistik                          ||")
-	fmt.Println("||  8. Tampilkan semua data                         ||")
-	fmt.Println("||  9. Keluar program                               ||")
-	fmt.Println("======================================================")
-	fmt.Print("Pilih menu (1-9): ")
-}
-
 func main() {
 	var input string
 
