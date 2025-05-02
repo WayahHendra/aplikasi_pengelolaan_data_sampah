153b0cd7bf71f8ffa13ff8c587625fa30aa67401
AphidZ <yogada017@gmail.com>
2025-05-02 10:35:29 +0700

    cleanup & add menu func

---
+++ cmd/function.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func addData() {
+	// TODO:
+	fmt.Println()
+}
+
+func updateData() {
+	// TODO:
+}
+
+func deleteData() {
+	// TODO:
+}
+
+func recordProcess() {
+	// TODO:
+}
+
+func searchData() {
+	// TODO:
+}
+
+func sortData() {
+	// TODO:
+}
+
+func showStatistics() {
+	// TODO:
+}
+
+func showAllData() {
+	// TODO:
+}
